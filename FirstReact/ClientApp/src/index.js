8841f5ae282cc6fafddbb89df8beea67287c68d9
Raizy Weissler <rwcprogramming@gmail.com>
2022-12-07 23:19:37 -0500

    Add project files.

---
+++ FirstReact/ClientApp/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import Peopletable from './Peopletable';
+
+ReactDom.render(< Peopletable />, document.getElementById('root'));
+
